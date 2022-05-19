0a6a9a45b2d364e0bf32595a6b83196fc9731089
eyupomer <odabasieyupomer@gmail.com>
2022-05-20 02:03:48 +0300

    keypress events added

--- script.js
+++ script.js
@@ -3,26 +3,44 @@ let inputScreen = document.getElementsByClassName("inputScreen")[0];
 let calculate = document.querySelector("#calculate");
 let clear = document.querySelector("#clear");
 
-for (var i = 0; i < 19; i++) {
-    if (i != 0 && i != 18) {
-        button[i].addEventListener("click", print);
-    }
-}
+
+// Functions
 
 function print() {
     inputScreen.value = inputScreen.value + this.value;
-}
+};
 
 function empty() {
     inputScreen.value = "";
 };
 
-clear.addEventListener("click", empty);
-
 function calculus() {
     inputScreen.value = eval(inputScreen.value);
-}
+};
+
+// Eventlisteners
+
+clear.addEventListener("click", empty);
+
+for (var i = 0; i < 19; i++) {
+    if (i != 0 && i != 18) {
+        button[i].addEventListener("click", print);
+    };
+};
 
 calculate.addEventListener("click", calculus);
 
 
+// Keypress Events
+
+document.addEventListener('keydown', function(e) {
+    if(e.key === 'Escape') {
+        empty();
+    }
+});
+
+document.addEventListener('keydown', function(e) {
+    if(e.key === 'Enter') {
+        inputScreen.value = eval(inputScreen.value);
+    }
+});
